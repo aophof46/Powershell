97897f5f6166fcafb6383f1bd5f8394bdc2a50e8
Adam <aophof46@users.noreply.github.com>
2018-07-05 21:53:20 -0400

    Create Create-TestFiles.ps1

---
+++ Misc/Create-TestFiles.ps1
@@ -0,0 +1,19 @@
+$SizeofFile = "100000000" # Size in Bytes, will be the maximum of the random number generated
+$NameOfFile =  "Dummy"
+$NumberOfFiles = "5"
+$LocationOfFiles = "C:\temp"
+
+if(!(Test-Path $LocationOfFiles))
+    {
+    New-Item -ItemType Directory $LocationOfFiles
+    }
+
+for($i = 0; $i -lt $NumberOfFiles; $i++)
+    {
+    $Size = Get-Random -Minimum 1 -Maximum $SizeofFile
+    $Path = $LocationOfFiles + '\' + $NameOfFile + '_' + $i + '.TestFile'
+    $File = [io.file]::Create($Path)
+    $File.SetLength($Size)
+    $File.Close()  
+    sleep 0.5
+    }
