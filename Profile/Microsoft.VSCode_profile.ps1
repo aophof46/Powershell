9da4831c47718a2c88758a78b93bc9dad48ed139
Adam <aophof46@users.noreply.github.com>
2021-02-17 10:55:49 -0500

    Update Microsoft.VSCode_profile.ps1
    added another documentation link

--- Profile/Microsoft.VSCode_profile.ps1
+++ Profile/Microsoft.VSCode_profile.ps1
@@ -4,6 +4,7 @@
 # $env:USERPROFILE\Documents\WindowsPowerShell\Microsoft.VSCode_profile.ps1
 
 # Command taken from https://www.reddit.com/r/PowerShell/comments/6oesbz/visual_studio_code_script_signing/
+# http://powershell.github.io/PowerShellEditorServices/guide/extensions.html#using-editor-commands
 Register-EditorCommand -Name SignCurrentScript -DisplayName 'Sign Current Script' -ScriptBlock {
     $cert = (Get-ChildItem Cert:\CurrentUser\My -CodeSigningCert)[0]
     $currentFile = $psEditor.GetEditorContext().CurrentFile.Path
