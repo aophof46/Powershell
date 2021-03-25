8a3e1a07415fbff1a427e2fb801d28d7bcb918b7
aophof46 <aophof46@gmail.com>
2021-03-25 09:39:40 -0400

    added os detection for different $env variables

--- Profile/Microsoft.PowerShell_profile.ps1
+++ Profile/Microsoft.PowerShell_profile.ps1
@@ -1,21 +1,41 @@
-if($($env:username) -like "<my management id>")
-    {
-	function prompt 
-	    {
-	    Write-Host "PS " -nonewline -foregroundcolor White
-	    Write-Host "[$Env:username]" -nonewline -foregroundcolor Red
-	    Write-Host " $PWD >" -nonewline -foregroundcolor White
-	    return " "
-	    } 
-    net use t: "\\path\to\drive\i\want\to\map"
+if($IsWindows) {
+	if($($env:username) -like "<my management id>") {
+		function prompt {
+			Write-Host "PS " -nonewline -foregroundcolor White
+			Write-Host "[$Env:username]" -nonewline -foregroundcolor Red
+			Write-Host " $PWD >" -nonewline -foregroundcolor White
+			return " "
+		} 
+		net use t: "\\path\to\drive\i\want\to\map"
 	}
-else
-	{
-	function prompt 
-	    {
-	    Write-Host "PS " -nonewline -foregroundcolor White
-	    Write-Host "[$Env:username]" -nonewline -foregroundcolor Green
-	    Write-Host " $PWD >" -nonewline -foregroundcolor White
-	    return " "
-	    } 
+	else {
+		function prompt {
+			Write-Host "PS " -nonewline -foregroundcolor White
+			Write-Host "[$Env:username]" -nonewline -foregroundcolor Green
+			Write-Host " $PWD >" -nonewline -foregroundcolor White
+			return " "
+		} 
 	}	
+}
+elseif($IsMacOS) {
+	if($($env:username) -like "<my management id>") {
+		function prompt {
+			Write-Host "PS " -nonewline -foregroundcolor White
+			Write-Host "[$env:USER]" -nonewline -foregroundcolor Red
+			Write-Host " $PWD >" -nonewline -foregroundcolor White
+			return " "
+		} 
+		net use t: "\\path\to\drive\i\want\to\map"
+	}
+	else {
+		function prompt {
+			Write-Host "PS " -nonewline -foregroundcolor White
+			Write-Host "[$env:USER]" -nonewline -foregroundcolor Green
+			Write-Host " $PWD >" -nonewline -foregroundcolor White
+			return " "
+		} 
+	}	
+}
+elseif($IsLinux) {
+
+}
