8584f7dfa5c80c2862ea6be6cd2b399dea8987a5
Adam <aophof46@users.noreply.github.com>
2022-09-07 10:18:29 -0400

    Update Test-Battery.ps1
    added process to generate load on CPU

--- Misc/Test-Battery.ps1
+++ Misc/Test-Battery.ps1
@@ -1,3 +1,32 @@
+<#
+$NumberOfLogicalProcessors = Get-WmiObject win32_processor | Select-Object -ExpandProperty NumberOfLogicalProcessors
+ 
+ForEach ($core in 1..$NumberOfLogicalProcessors){ 
+ 
+start-job -ScriptBlock{
+ 
+    $result = 1;
+    foreach ($loopnumber in 1..2147483647){
+        $result=1;
+        
+        foreach ($loopnumber1 in 1..2147483647){
+        $result=1;
+            
+            foreach($number in 1..2147483647){
+                $result = $result * $number
+            }
+        }
+ 
+            $result
+        }
+    }
+}
+ 
+Read-Host "Press any key to exit..."
+Stop-Job * 
+#>
+
+
 $resultsarray = @()
 do {
   $row = New-Object psobject
